55c437f683a90e7a7afe951348651465df5a7851
Juan Carlos Álvarez <jucaalpa@gmail.com>
2017-06-09 17:36:52 -0500

    aa

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,7 @@
 
 'use strict';
 
-const applicationServerPublicKey = 'BB173l8rJtk3Q2PUyoCNHszuZ7EHUZdSRFQFFUwuHQB9X0AmssVKloJZuu6+9L9MnCI6piKFCZuIsyeGz9nWfcc='; 
+const applicationServerPublicKey = 'BLoGoXqUAY+LKyVEOGRaOUxid3cCq0/bUVNmvkpCDop3Z0rh4KR1b5MCpv1+3d3NBSmAw3gSHqMiShxHJ4Exj+A='; 
 
 const pushButton = document.querySelector('.js-push-btn');
 
